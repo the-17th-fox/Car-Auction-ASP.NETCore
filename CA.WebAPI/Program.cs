bd8cad0268af45d9b8a46ec2138e0ba1660dc1d4
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-04-19 19:04:58 +0300

    Added a context file and a configuration for the context and Identity

--- CA.WebAPI/Program.cs
+++ CA.WebAPI/Program.cs
@@ -1,6 +1,21 @@
+using CA.Domain.Entities;
+using CA.Persistence.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AuctionContext>(context => context.UseSqlServer(builder.Configuration["connectionStrings:DatabaseConnection"]));
+
+builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+    options.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<AuctionContext>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -8,7 +23,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ Infrastructure/CA.Persistence/Context/AuctionContext.cs
@@ -0,0 +1,36 @@
+﻿using CA.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.Persistence.Context
+{
+    public class AuctionContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<Lot> Lots { get; set; }
+        public DbSet<LotBid> Bids { get; set; }
+        public DbSet<Transaction> UsersBalances { get; set; }
+
+        // Add shadow property for the user: account created at, updated at
+
+        public AuctionContext(DbContextOptions<AuctionContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Car>().Property<DateTime>("CreatedAt");
+            builder.Entity<Car>().Property<DateTime>("UpdatedAt");
+
+            builder.Entity<User>().Property<DateTime>("CreatedAt");
+            builder.Entity<User>().Property<DateTime>("UpdatedAt");
+        }
+    }
+}
