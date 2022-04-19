7c890e0135dabeebf5a64369e5df95bb5066af53
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-04-19 18:57:26 +0300

    Added exceptions handling middleware class and various errors implementation

---
+++ Core/CA.Domain/GlobalExceptions/InvalidCredentialsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.Domain.GlobalExceptions
+{
+    public class InvalidCredentialsException : Exception
+    {
+        public InvalidCredentialsException(string message = "Provided credentials are invalid.") : base(message) { }
+    }
+}
---
+++ Core/CA.Domain/GlobalExceptions/UnknownErrorException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.Domain.GlobalExceptions
+{
+    public class UnknownErrorException : Exception
+    {
+        public UnknownErrorException(string message = "An unknown error has occured.") : base(message) { }
+    }
+}
