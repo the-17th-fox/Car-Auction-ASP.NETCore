65bf43735332b2c8d78a86c85a2f46af5897e287
Nikita <82900624+the-17th-fox@users.noreply.github.com>
2022-04-20 19:07:02 +0300

    feature: Implement global errors handling middleware (#1)
    * Added exceptions handling middleware class and various errors implementation
    
    * Added configuration of the middleware to the program file

---
+++ Core/CA.Domain/GlobalExceptions/UpdatingFailedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.Domain.GlobalExceptions
+{
+    public class UpdatingFailedException : Exception
+    {
+        public UpdatingFailedException(string message = "An error occured during the updating.") : base(message) { }
+    }
+}
---
+++ Core/CA.Domain/Middlewares/ExceptionsHandlingMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using CA.Domain.GlobalExceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CA.Domain.Middlewares
+{
+    public class ExceptionsHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionsHandlingMiddleware(RequestDelegate next) => _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exeption)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                response.StatusCode = exeption switch
+                {
+                    NotFoundException => (int)HttpStatusCode.NotFound,
+
+                    InvalidCredentialsException => (int)HttpStatusCode.Unauthorized,
+
+                    _ => (int)HttpStatusCode.InternalServerError,
+                };
+
+                var exeptionResponse = new
+                {
+                    message = exeption.Message,
+                    statusCode = response.StatusCode
+                };
+
+                var result = JsonSerializer.Serialize(exeptionResponse);
+                await response.WriteAsync(result);
+            }
+        }
+
+    }
+}
